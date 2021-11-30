0931542f745d620a0754718a00e95ad067b53134
Mark Thurston <mark@mdvthu.com>
2021-11-30 20:55:52 +0000

    Base configuration (in Python format)

---
+++ config/config.py
@@ -0,0 +1,13 @@
+# How many samples of the word list for the final password?
+N = 3
+# How to join each word
+SEPARATOR = '-'
+# Maximum number of chars. in each word
+MAX_LEN = 5
+
+# cross-platform word list locations
+# (adjust this, if a smaller vocab is preferred)
+WORD_LISTS = [
+    '/usr/share/dict/british-english', '/usr/share/dict/words',
+    '/usr/share/dict/web2'
+]
