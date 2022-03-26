b2bd64a8bff7b18a71e2d6268070db1ef55d8988
Mark Thurston <mark@mdvthu.com>
2022-03-26 14:28:57 +0000

    Update Licence and requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup
 
-with open('LICENCE') as f:
-    licence = f.read()
-
 setup(name='mempw',
-      version='0.1.3',
+      version='0.1.4',
       description='Python-based memorable password generator',
       long_description=('Generates pronouncable passwords '
                         'based on system wordlists'),
       author='Mark Thurston',
       author_email='mark@mdvthu.com',
       url='https://github.com/mdvthu/mempw',
-      license=licence,
+      license_files=('LICENCE'),
       packages=['mempw'],
+      install_requires=['appdirs'],
       entry_points={'console_scripts': ['mempw=mempw.core:cli']})
