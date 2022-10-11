b7a35d86ea3407cee777e0f24f1552c4c61ba92a
Mark Thurston <mark@mdvthu.com>
2022-10-11 21:10:13 +0100

    Version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mempw',
-      version='0.1.4',
+      version='0.1.5',
       description='Python-based memorable password generator',
       long_description=('Generates pronouncable passwords '
                         'based on system wordlists'),
