30ac618728ac99bebe325e6400d5c02125ff9e19
Mark Thurston <mark@mdvthu.com>
2022-10-12 20:48:28 +0100

    Increment version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mempw',
-      version='0.1.5',
+      version='0.1.6',
       description='Python-based memorable password generator',
       long_description=('Generates pronouncable passwords '
                         'based on system wordlists'),
